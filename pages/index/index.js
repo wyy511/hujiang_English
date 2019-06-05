fcf46b49f1e9f7e054d0f80ad4cf293da20a09e6
wuyaoyao <wuyaoyao@xiniaoyun.com>
2019-06-05 16:25:09 +0800

    添加mock，底部导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,8 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var API = require('../../mock/api.js')
+console.log(API)
 
 Page({
   data: {
@@ -16,6 +18,17 @@ Page({
     })
   },
   onLoad: function () {
+    var that = this
+    // 使用 Mock
+    API.ajax('', function (res) {
+      //这里既可以获取模拟的res
+      console.log(res)
+      that.setData({
+        list: res.data
+      })
+    });
+
+    console.log(this.data.list)
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
